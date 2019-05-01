71f2f9e2dd24e8c073b9733f5395c8eae69a987a
marynelko <marynelko@gmail.com>
2019-05-01 19:35:03 +0200

    Parallel examples

---
+++ src/testClasses/TestNG_TestParallel1.java
@@ -0,0 +1,19 @@
+package testClasses;
+
+import org.testng.annotations.Test;
+
+public class TestNG_TestParallel1 {
+  @Test
+  public void TestMethod1() throws InterruptedException {
+	  System.out.println("TestNG_TestParallel1 -> TestMethod1");
+	  Thread.sleep(6000);
+	  System.out.println("TestNG_TestParallel1 -> TestMethod1 -> More steps");
+  }
+  
+  @Test
+  public void TestMethod2() throws InterruptedException {
+	  System.out.println("TestNG_TestParallel1 -> TestMethod2");
+	  Thread.sleep(6000);
+	  System.out.println("TestNG_TestParallel1 -> TestMethod2 -> More steps");
+  }
+}
